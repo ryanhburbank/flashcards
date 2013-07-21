c86425829870bfb06907a2e211c10cf6ffa8b7e0
jmyers0022 <jmyers0022@gmail.com>
2013-07-21 11:58:29 -0500

    working on update view

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,10 +1,26 @@
 get '/deck/new' do
-  @decks = Deck.all
   erb :'/deck/new'
 end
 
+get '/deck/update' do 
+  @decks = Deck.all
+  erb :'/deck/update'
+end
+
+post '/deck/update' do
+  @current_deck = Deck.find_by_title(deck[:title])
+  redirect to("/deck/#{@current_deck.id}")
+end
+
+get '/deck/update/:deck' do
+  @current_deck = Deck.find(params[:deck])
+  @current_card = @current_deck.cards.find_by_question(card[:question])
+  erb :'/deck/update'
+end
+
+
 get '/deck/new/:question' do
-  @question = Deck.find_by_question(params[:question])
+ 
   erb :'/deck/new'
 end  
 
