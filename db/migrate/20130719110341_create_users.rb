7fb97f06695c00adfca612e1ea234ca4ea7bffa8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-20 20:28:18 -0500

    Add bncrypt to migration

--- db/migrate/20130719110341_create_users.rb
+++ db/migrate/20130719110341_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :last_name
       t.string :user_name
       t.string :email
-      t.string :password_hash
+      t.string :password_digest
 
       t.timestamps
     end
